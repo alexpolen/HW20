d7851436fc00cbe85df1064daa56485b458c2285
alexpolen <101073374+alexpolen@users.noreply.github.com>
2022-09-07 17:01:16 -0600

    a
    a

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log("YOU DID IT");
+
+
+
+
+
+document.querySelectorAll('input[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
+
+document.querySelectorAll('li[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
+
+
+
